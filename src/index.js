61bfc24d65326d30cfd06037102df7431fa3e8c8
jwki0071 <jwki0071@gmail.com>
2018-09-25 16:11:34 -0400

    map in progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Happy Tuesday');
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVld29ua2ltIiwiYSI6ImNqbWk1ZGR4OTAxZWczcG1wZ3RodDRqeDUifQ.Lo_5whNTHoAU-9XnG_55oQ';
+var map = new mapboxgl.Map({
+container: 'YOUR_CONTAINER_ELEMENT_ID',
+style: 'mapbox://styles/mapbox/streets-v10'
+});
